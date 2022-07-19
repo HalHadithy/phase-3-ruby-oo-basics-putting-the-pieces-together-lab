92e0871e1d907075db81c31885d33a6df1bd644b
Haala <haalahadithy@gmail.com>
2022-07-19 10:28:17 -0400

    commit tree 84416ea2003bc103e2fff261f522d136150aeb4c

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,44 @@
+class Book
+    attr_reader :title 
+
+    def initalize (title)
+        @title = title
+    end 
+
+    def title
+        @title
+    end
+
+
+    def initalize
+        @author_name = ""
+    end
+
+    def initalize
+        @page_count = 0
+    end
+
+    def turn_page
+        @page_count += 1
+    end
+
+
+    def initalize
+        @genre = ""
+    end
+
+    def print_book_details
+        puts "Title: #{@title}"
+        puts "Author: #{@author_name}"
+        puts "Genre: #{@genre}"
+        puts "Page: #{@page_count}"
+    end
+   
+
+    
+end
+
+
+
 
 
